41437a91c346c80c5db1c24aa4253f6510ca9258
Fraser-Greenlee <fraser.greenlee@icloud.com>
2022-04-13 11:59:30 +0100

    new attention block tests

--- tests/test_modeling_funnel_flax.py
+++ tests/test_modeling_funnel_flax.py
@@ -7,7 +7,8 @@ from transformers import FunnelConfig, FunnelTokenizer
 
 from model.modeling_funnel_flax import (
     FunnelEmbeddings,
-    FunnelAttentionStructure
+    FunnelAttentionStructure,
+    FunnelRelMultiheadAttention,
 )
 
 
@@ -131,3 +132,98 @@ class TestFunnelAttentionStructure(unittest.TestCase):
         self.assertEqual(token_type_mat.shape, (2, 2, 2))
         self.assertEqual(attention_mask.shape, (2, 2))
         self.assertEqual(cls_mask.shape, (2, 2))
+
+
+class TestFunnelRelMultiheadAttention(unittest.TestCase):
+
+    def test_default_block_0(self):
+        batch_size, seq_len = 2, 4
+        config = FunnelConfig()
+        inputs_embeds  = jax.random.normal(rng, (batch_size, seq_len, config.d_model))
+        attention_mask = jnp.array([ [1,1,1,0], [1,1,1,1] ])
+        token_type_ids = jnp.array([ [2,1,1,1], [2,2,1,1] ])
+        attention_structure = FunnelAttentionStructure(config)
+        variables = attention_structure.init(
+            rng, inputs_embeds, attention_mask, token_type_ids, method=attention_structure.init_attention_inputs
+        )
+        attention_inputs = attention_structure.apply(
+            variables, inputs_embeds, attention_mask, token_type_ids, method=attention_structure.init_attention_inputs
+        )
+        query = jax.random.normal(rng, (batch_size, seq_len, config.d_model))
+        key =   jax.random.normal(rng, (batch_size, seq_len, config.d_model))
+        value = jax.random.normal(rng, (batch_size, seq_len, config.d_model))
+        #
+        attention = FunnelRelMultiheadAttention(config, 0)
+        variables = attention.init(
+            rng, query, key, value, attention_inputs, output_attentions=True
+        )
+        output, attn_prob = attention.apply(
+            variables, query, key, value, attention_inputs, output_attentions=True
+        )
+        self.assertEqual(output.shape,      (2, 4, 768))
+        self.assertEqual(attn_prob.shape,   (2, 12, 4, 4))
+        self.assertEqual(attn_prob[0,0,:,-1].tolist(), [0.0, 0.0, 0.0, 0.0])
+
+    def test_default_block_1(self):
+        batch_size, seq_len = 2, 7
+        config = FunnelConfig(d_model=32, n_head=4, d_head=8)
+        inputs_embeds  = jax.random.normal(rng, (batch_size, seq_len, config.d_model))
+        attention_mask = jnp.array([ [1,1,1,1,1,1,0], [1,1,1,1,1,0,0] ])
+        token_type_ids = jnp.array([ [2,1,1,1,1,1,1], [2,2,1,1,1,1,1] ])
+        attention_structure = FunnelAttentionStructure(config)
+        variables = attention_structure.init(
+            rng, inputs_embeds, attention_mask, token_type_ids, method=attention_structure.init_attention_inputs
+        )
+        attention_inputs = attention_structure.apply(
+            variables, inputs_embeds, attention_mask, token_type_ids, method=attention_structure.init_attention_inputs
+        )
+        pooled_hidden, new_attention_inputs = attention_structure.apply(
+            variables, inputs_embeds, attention_inputs, method=attention_structure.pre_attention_pooling
+        )
+        query = jax.random.normal(rng, pooled_hidden.shape)
+        key =   jax.random.normal(rng, inputs_embeds.shape)
+        value = jax.random.normal(rng, inputs_embeds.shape)
+        #
+        attention = FunnelRelMultiheadAttention(config, 1)
+        variables = attention.init(
+            rng, query, key, value, new_attention_inputs, output_attentions=True
+        )
+        output, attn_prob = attention.apply(
+            variables, query, key, value, new_attention_inputs, output_attentions=True
+        )
+        self.assertEqual(output.shape, (2, 4, 32))
+        self.assertEqual(attn_prob.shape, (2, 4, 4, 7))
+        self.assertEqual(attn_prob[:,:,:,-1].tolist(), [[[0.0, 0.0, 0.0, 0.0] for _ in range(4)] for _ in range(2)])
+        self.assertEqual(attn_prob[1,:,:,-2].tolist(), [[0.0, 0.0, 0.0, 0.0] for _ in range(4)])
+        self.assertNotEqual(attn_prob[0,:,:,-2].tolist(), [[0.0, 0.0, 0.0, 0.0] for _ in range(4)])
+
+    def test_not_pool_q_only_block_1(self):
+        # Failing
+        batch_size, seq_len = 2, 7
+        config = FunnelConfig(pool_q_only=False, d_model=32, n_head=4, d_head=8)
+        inputs_embeds  = jax.random.normal(rng, (batch_size, seq_len, config.d_model))
+        attention_mask = jnp.array([ [1,1,1,1,1,1,0], [1,1,1,1,1,0,0] ])
+        token_type_ids = jnp.array([ [2,1,1,1,1,1,1], [2,2,1,1,1,1,1] ])
+        attention_structure = FunnelAttentionStructure(config)
+        variables = attention_structure.init(
+            rng, inputs_embeds, attention_mask, token_type_ids, method=attention_structure.init_attention_inputs
+        )
+        attention_inputs = attention_structure.apply(
+            variables, inputs_embeds, attention_mask, token_type_ids, method=attention_structure.init_attention_inputs
+        )
+        pooled_hidden, new_attention_inputs = attention_structure.apply(
+            variables, inputs_embeds, attention_inputs, method=attention_structure.pre_attention_pooling
+        )
+        query = jax.random.normal(rng, pooled_hidden.shape)
+        key =   jax.random.normal(rng, inputs_embeds.shape)
+        value = jax.random.normal(rng, inputs_embeds.shape)
+        #
+        attention = FunnelRelMultiheadAttention(config, 1)
+        variables = attention.init(
+            rng, query, key, value, new_attention_inputs, output_attentions=True
+        )
+        output, attn_prob = attention.apply(
+            variables, query, key, value, new_attention_inputs, output_attentions=True
+        )
+        breakpoint()
+        z = 1
